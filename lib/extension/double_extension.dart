b20e8fc0fc8db7e54a8264a6aa1a43dc4edb488b
zainpelope <palengaancity12345@gmail.com>
2022-11-27 21:22:13 +0700

    Responsive UI

--- lib/extension/double_extension.dart
+++ lib/extension/double_extension.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 extension DoubleExtension on double {
-  Widget get width => SizedBox(width: this);
-  Widget get height => SizedBox(height: this);
+  Widget get width => SizedBox(width: w);
+  Widget get height => SizedBox(height: h);
 
 }
