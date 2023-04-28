8802abf26ce7092b533882631cdf77d76a3f7e28
zainpelope <palengaancity12345@gmail.com>
2023-04-28 10:29:05 +0700

    import assets

---
+++ lib/data/src/img_string.dart
@@ -0,0 +1,34 @@
+class ImgString {
+  static const String standing2 ='assets/images/Città Standing-2.png';
+  static const String standing1 = 'assets/images/Città Standing-1.png';
+  static const String standing = 'assets/images/Città Standing.png';
+  static const String plants6 = 'assets/images/Città Plants-6.png';
+  static const String plants5 = 'assets/images/Città Plants-5.png';
+  static const String plants4 = 'assets/images/Città Plants-4.png';
+  static const String abstract3 = 'assets/images/Città Abstract-3.png';
+  static const String livingRoom1 = 'assets/images/Città Living Room-1.png';
+  static const String partyTime1 = 'assets/images/Città Party Time-1.png';
+  static const String house = 'assets/images/Città House.png';
+  static const String coffeeShop2 = 'assets/images/Città Coffee Shop-2.png';
+  static const String giantPhone1 = 'assets/images/Città Giant Phone-1.png';
+  static const String avatar5 = 'assets/images/Città Avatar-5.png';
+  static const String plants3 = 'assets/images/Città Plants-3.png';
+  static const String plants2 = 'assets/images/Città Plants-2.png';
+  static const String avatarAndBackground =
+      'assets/images/Città Avatar and Backdrop.png';
+  static const String plants1 = 'assets/images/Città Plants-1.png';
+  static const String plants = 'assets/images/Città Plants.png';
+  static const String avatar4 = 'assets/images/Città Avatar-4.png';
+  static const String avatar3 = 'assets/images/Città Avatar-3.png';
+  static const String avatar2 = 'assets/images/Città Avatar-2.png';
+  static const String abstract2 = 'assets/images/Città Abstract-2.png';
+  static const String abstract1 = 'assets/images/Città Abstract-1.png';
+  static const String partyTime = 'assets/images/Città Party Time.png';
+  static const String coffeeShop1 = 'assets/images/Città Coffee Shop-1.png';
+  static const String avatar1 = 'assets/images/Città Avatar-1.png';
+  static const String giantPhone = 'assets/images/Città Giant Phone.png';
+  static const String abstract = 'assets/images/Città Abstract.png';
+  static const String avatar = 'assets/images/Città Avatar.png';
+  static const String livingRoom = 'assets/images/Città Living Room.png';
+  static const String coffeeShop = 'assets/images/Città Coffee Shop.png';
+}
