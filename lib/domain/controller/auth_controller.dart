2585077558532a58dc5938aa88746e4af18dc138
zainpelope <palengaancity12345@gmail.com>
2023-05-01 13:40:26 +0700

    http state

--- lib/domain/controller/auth_controller.dart
+++ lib/domain/controller/auth_controller.dart
@@ -8,16 +8,24 @@ import '../../config/networking/base_response.dart';
 
 class AuthController implements AuthRepository {
   final ApiClient _client = ApiClient();
+  final HttpState _httpState;
+
+  AuthController(this._httpState);
   @override
   Future<BaseResponse> forgotPassword(String email) async {
+    String method = "POST";
+    String url = Endpoint.forgotPassword;
+    _httpState.onStartRequest(url, method);
     late final BaseResponse baseResponse;
-    final response =
-        await _client.post(Uri.parse(Endpoint.forgotPassword), body: {
+    final response = await _client.post(Uri.parse(url), body: {
       'email': email,
     });
+    _httpState.onEndRequest(url, method);
     if (response.statusCode < 500) {
+      _httpState.onSuccessRequest(url, method);
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
+      _httpState.onErrorRequest(url, method);
       baseResponse = BaseResponse(message: response.body);
     }
     return BaseResponse.fromJson(response);
@@ -25,12 +33,21 @@ class AuthController implements AuthRepository {
 
   @override
   Future<BaseResponse> login(String email, String password) async {
+    String method = "POST";
+    String url = Endpoint.login;
+    _httpState.onStartRequest(url, method);
     late final BaseResponse baseResponse;
-    final response = await _client.post(Uri.parse(Endpoint.login), body: {
+    final response = await _client.post(Uri.parse(url), body: {
       'email': email,
       'password': password,
     });
+    _httpState.onErrorRequest(url, method);
     if (response.statusCode < 500) {
+      if (response.statusCode >= 199 && response.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        (_httpState.onErrorRequest(url, method));
+      }
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
       baseResponse = BaseResponse(message: response.body);
@@ -40,22 +57,46 @@ class AuthController implements AuthRepository {
 
   @override
   Future<void> logout() async {
+    String method = "POST";
+    String url = Endpoint.logout;
+    _httpState.onStartRequest(url, method);
     late final BaseResponse baseResponse;
-    await _client.post(Uri.parse(Endpoint.logout), body: {});
+    await _client.post(Uri.parse(url), body: {}).then((value) {
+      if (value.statusCode >= 199 && value.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        (_httpState.onErrorRequest(url, method));
+      }
+    }).catchError((onError) {
+      _httpState.onErrorRequest(url, method);
+    });
+    _httpState.onEndRequest(url, method);
   }
 
   @override
   Future<BaseResponse> register(
       String email, String password, String name) async {
+    String method = "POST";
+    String url = Endpoint.register;
+    _httpState.onStartRequest(url, method);
     late final BaseResponse baseResponse;
-    final response = await _client.post(Uri.parse(Endpoint.register), body: {
+    final response = await _client.post(Uri.parse(url), body: {
       'email': email,
       'password': password,
       'name': name,
     });
+
+    _httpState.onEndRequest(url, method);
     if (response.statusCode < 500) {
+      if (response.statusCode >= 199 && response.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        (_httpState.onErrorRequest(url, method));
+      }
+
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
+      _httpState.onErrorRequest(url, method);
       baseResponse = BaseResponse(message: response.body);
     }
     return BaseResponse.fromJson(response);
